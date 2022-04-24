66a0fa21cc269eb0d4e2a300e0418d543ff5871e
lsfex <luis@rota-livre.com>
2022-04-24 17:42:11 -0300

    pacote ok

---
+++ build/app/generated/source/buildConfig/release/online/edentech/fuodz/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package online.edentech.fuodz;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.morungaba.usuario";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 32;
+  public static final String VERSION_NAME = "1.5.3";
+}
