fc7a127c9111c10b698e071113f3a92c54f56668
lsfex <luis@rota-livre.com>
2022-04-28 22:14:52 -0300

    infoplist

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -15,3 +15,54 @@ import GoogleMaps
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+    
+// AppDelegate.swift
+import UIKit
+import FacebookCore
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {          
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+
+        return true
+    }
+          
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
+    }  
+}
+
+
+// SceneDelegate.swift
+import FacebookCore
+  ...
+func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    guard let url = URLContexts.first?.url else {
+        return
+    }
+
+    ApplicationDelegate.shared.application(
+        UIApplication.shared,
+        open: url,
+        sourceApplication: nil,
+        annotation: [UIApplication.OpenURLOptionsKey.annotation]
+    )
+}
+
